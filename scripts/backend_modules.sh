7206202cb9275add42568e24655b2671ba55e780
Juan Mahecha <juanmahecha@iMac-de-Juan.local>
2021-05-29 20:34:38 -0500

    scripts

--- scripts/backend_modules.sh
+++ scripts/backend_modules.sh
@@ -2,14 +2,22 @@
 set -fueo pipefail
 
 function main() { (
-    echo "node modules download...."
+    echo "Backend by juanmahecha9, created with Node: 14.17.0"
     cd backend
-    npm install --save bcrypt consign cors express express-myconnection fs json jsonwebtoken morgan nodemailer path pg pg-hstore rimraf sequelize
-    npm install --save-dev @babel/cli @babel/core @babel/node @babel/preset-env nodemon
-    echo "Download success..."
-    echo "..................."
-    echo "Initializing the server"
-    sh scripts/run.sh
+    DIRECTORIO=node_modules
+    if [ -d "$DIRECTORIO" ]; then
+        echo "..................."
+        echo "Initializing the server"
+        sh scripts/run.sh
+    else
+        echo "node modules download...."
+        npm install --save bcrypt consign cors express express-myconnection fs json jsonwebtoken morgan nodemailer path pg pg-hstore rimraf sequelize
+        npm install --save-dev @babel/cli @babel/core @babel/node @babel/preset-env nodemon
+        echo "Download success..."
+        echo "..................."
+        echo "Initializing the server"
+        sh scripts/run.sh
+    fi
 ); }
 
 main
--- scripts/frontend_modules.sh
+++ scripts/frontend_modules.sh
@@ -2,13 +2,21 @@
 set -fueo pipefail
 
 function main() { (
-    echo "node modules download...."
+    echo "Frontend by juanmahecha9, created with Angular CLI: 12.0.2"
     cd frontend
-    npm install
-    echo "Download success..."
-    echo "..................."
-    echo "Initializing the server"
-    sh scripts/run.sh
+    DIRECTORIO=node_modules
+    if [ -d "$DIRECTORIO" ]; then
+        echo "..................."
+        echo "Initializing the server"
+        sh scripts/run.sh
+    else
+        echo "node modules download...."
+        npm install
+        echo "Download success..."
+        echo "..................."
+        echo "Initializing the server"
+        sh scripts/run.sh
+    fi
 ); }
 
 main
